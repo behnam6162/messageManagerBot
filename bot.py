31db2f1f896ff9815836125c69c8160269d549f4
behnam6162 <76770471+behnam6162@users.noreply.github.com>
2022-11-13 01:19:28 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -118,9 +118,7 @@ async def messageManager(client, message):
                         await bot.send_message(-1001462419183, msg)
 
                 elif str(message.chat.type) == "ChatType.SUPERGROUP":
-                    if hasattr(message, "from_user") and message.from_user.is_bot:
-                        pass
-                    elif str(message.chat.username) != "None":
+                    if str(message.chat.username) != "None":
                         msg += "\n\n" + "لينک گروه:" + "\n\n" + "@" + str(message.chat.username)
                         msg += "\n\n" + "لينک پيام:" + "\n\n" + "https://t.me/%s/%s" % (str(message.chat.username), str(message.id))
                         
